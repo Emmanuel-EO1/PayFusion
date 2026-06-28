8bb20416a53fd9dc487c8b7f4ed58066d26e2df9
SOJ <olaleyetolu2003@gmail.com>
2026-06-28 13:33:55 +0100

    feat: Phase 10 Step 4

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 import logging
 
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.db.models import Prefetch, Sum, Count
 
